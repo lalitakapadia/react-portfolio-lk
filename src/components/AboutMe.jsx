2880702bc6741140a077e7a184c8ac7c67396ca5
lalitakapadia <145744219+lalitakapadia@users.noreply.github.com>
2024-02-25 13:28:46 +0000

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,16 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 export default function AboutMe() {
     return (
-        <div style = {{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
-            <div style = {{displya: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px', maxWidth: '600px', width: '100%' }} >
-            <img src= "./src/assets/images/lalitakapadia.jpg" alt = "photo"  
-            style={{ width: '95px', height: '95px', borderRadius: '50%' }} ></img>
-            <div style = {{margin: '20px 0'}} >
-                <h3 style = {{ textAlign: 'left', fontSize: '1.5em'}}>About Me</h3>
-            </div>
-        </div>
-        <p style={{ textAlign: 'justify', fontSize: '1.3em', maxWidth: '90%', width: '100%' }}>
-            paragraph about me 
-        </p>
-        </div>
+        <Container>
+            <Row>
+                <Col sm={4}>
+                    <Image src="./src/assets/images/lalitakapadia.jpg" roundedCircle fluid />
+                    {/* <img src= "./src/assets/images/lalitakapadia.jpg" alt = "photo"  
+                    style={{ width: '195px', height: '195px', borderRadius: '50%' }} ></img> */}
+                </Col>
+                <Col sm={8}>
+                    <h3 style = {{ textAlign: 'left', fontSize: '1.5em'}}>About Me</h3>
+                    <p style={{ textAlign: 'justify', fontSize: '1.3em', maxWidth: '90%', width: '100%' }}>
+                     paragraph about me 
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
