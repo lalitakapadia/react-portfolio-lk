97897e0d3e12c275769c1ed8934bc9207ea1544e
lalitakapadia <145744219+lalitakapadia@users.noreply.github.com>
2024-02-25 13:29:29 +0000

    Update NavTabs.jsx

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,71 +1,26 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
-  const linkStyle = {
-    color: '#1E2749',
-    textDecoration: 'none',
-    padding: '8px',
-    borderRadius: '5px',
-  };
-
-  const activeLinkStyle = {
-    backgroundColor: '#1E2749',
-    color: '#BCD2EE',
-  };
-
   return (
-    <div className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#BCD2EE' }}>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center">
-        <div>
-          <h1 style={{ color: '#1E2749', textAlign: 'left', fontFamily: 'Cursive', fontWeight: 'bold' }}>
-            Lalita Kapadia
-          </h1>
-        </div>
-        <nav className="space-x-4" id="navbarNav">
-          <ul className="flex navbar-nav" style={{ marginLeft: 'auto' }}>
-            <li className="nav-item p-2">
-              <Link
-                to="/"
-                className="nav-link"
-                style={{ ...linkStyle, ...(currentPage === '/' && activeLinkStyle) }}
-              >
-                About Me
-              </Link>
-            </li>
-            <li className="nav-item p-2">
-              <Link
-                to="/Portfolio"
-                className="nav-link"
-                style={{ ...linkStyle, ...(currentPage === '/Portfolio' && activeLinkStyle) }}
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li className="nav-item p-2">
-              <Link
-                to="/Contact"
-                className="nav-link"
-                style={{ ...linkStyle, ...(currentPage === '/Contact' && activeLinkStyle) }}
-              >
-                Contact
-              </Link>
-            </li>
-            <li className="nav-item p-2">
-              <Link
-                to="/Resume"
-                className="nav-link"
-                style={{ ...linkStyle, ...(currentPage === '/Resume' && activeLinkStyle) }}
-              >
-                Resume
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+
+    <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/aboutme">Lalita Kapadia</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/aboutme">About Me</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
