a4145ddbbdc1b514a6d4cfe7ead43c7d1f4e5799
lalitakapadia <lalita.kapadia@gmail.com>
2024-02-27 23:20:22 +0000

    react bootstrap portfolio and contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import {React, useState } from 'react';
+import { validateEmail } from '../utils/helpers';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -17,47 +17,49 @@ export default function Contact() {
         setValidated(true);
       };
     return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Col className="p:10" xs={11} sm={11}>
-                <Row className="mb-3">
-                    <Row className="mb-3">
-                        <Form.Group md="4" controlId="validationCustom01">
-                            <Form.Label>First name</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="Full Name"
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Please enter your name
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Row>
-                <Row className="mb-3">
-                    <Form.Group controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" required placeholder="Enter email" />
-                            <Form.Control.Feedback type="invalid">
-                                Please enter valid email address
-                            </Form.Control.Feedback>
-                        </Form.Group>
+        <div class='p-10'>
+            <Form bg="dark" data-bs-theme="dark"  noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-10">
+                    <Col xs={4} sm={{ span: 4, offset: 4 }}>
+                        <Row className="mb-3">
+                            <Form.Group md="4" controlId="validationCustom01">
+                                <Form.Label>First name</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="Full Name"
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your name
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Row>
+                        <Row className="mb-3">
+                            <Form.Group controlId="formGridEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" required placeholder="Enter email" />
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter valid email address
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                        </Row>
+                        <Row className="mb-3">
+                            <Form.Group controlId="formGridMessage">
+                                    <Form.Label>Message</Form.Label>
+                                    <Form.Control required as="textarea" rows={3}  placeholder="Enter your message here" />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please enter the message
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                        </Row>
+                        <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridSubmit">
+                                    <Button type="submit" variant="primary">Submit</Button>{' '}
+                                </Form.Group>
+                        </Row>
+                    </Col>
                 </Row>
-                <Row className="mb-3">
-                    <Form.Group controlId="formGridMessage">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control required as="textarea" rows={3}  placeholder="Enter your message here" />
-                            <Form.Control.Feedback type="invalid">
-                                Please enter the message
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                </Row>
-                <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridSubmit">
-                            <Button type="submit" variant="primary">Submit</Button>{' '}
-                        </Form.Group>
-                </Row>
-                </Row>
-            </Col>
-        </Form>
+            </Form>
+        </div>
     )     
 }
